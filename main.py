0f254070c8ac6b0fed1e2125ea8f6af6e5391f85
Ashutosh kumar gautam <65886012+Ashutoshkumargautam@users.noreply.github.com>
2020-11-14 15:22:50 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-
+#!/usr/bin/env python
+import time
+import sys
+import re
+import os
+#==========================================================================================
+#taking first information file location here...
+user_input_0 = raw_input("Enter the path of your file: ")
+#now taking user own wordlist file location here....
+user_input_1 = raw_input("Enter the path of your own wordlist file: ")
+#====[user_input[1]]========================================================================================
+assert os.path.exists(user_input_0), " [+] I did not find the file at, "+str(user_input_0)
+f = open(user_input_0,'r+')
+print(" [+] File founded")
+f.close()
+#====[user_input[2]]========================================================================================
+assert os.path.exists(user_input_1), " [+] I did not find the file at, "+str(user_input_1)
+f = open(user_input_1,'r+')
+print(" [+] File founded")
+f.close()
+#========[Reading file here[0]]========================================================================
+f = open(str(user_input_0),"r+")
+print(f.read())
+f.close()
+#========[Reading file here[1]]========================================================================
+f = open(str(user_input_1),"r+")
+print(f.read())
+f.close()
+#==========================================================================================
